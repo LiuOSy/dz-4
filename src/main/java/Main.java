2b756a3e2aef11672e1d92c2900a7fafca80d84f
Liudmyla Sydorenko <mila_1@ukr.net>
2023-06-17 22:50:29 +0300

    Adding java code

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int b = new SecondClass().printInConsole();
+        System.out.println(String.format("~~~<%d>~~~", b));
+    }
+}
---
+++ src/main/java/SecondClass.java
@@ -0,0 +1,10 @@
+public class SecondClass {
+
+    int a = 5;
+
+    int printInConsole(){
+        System.out.println(String.format("---<%d>---", a));
+        return a;
+    }
+
+}
